993bc0a7ea5d5f4d39faac49cf0ab8dc4451ce7b
Chauntae Johnson <jchauntae@gmail.com>
2023-09-18 17:42:00 -0400

    My server is up and running

---
+++ views/Show.jsx
@@ -0,0 +1,7 @@
+// const react = require("react");
+// class Show extends React.Component {
+//   render() {
+//     return <h1> 'Welcome to the Pokemon App!'</h1>;
+//   }
+// }
+// module.exports = Show;
